cf7da62e558dc6c458752ab62de54f08809d6609
LurdKek <casperlerka@gmail.com>
2017-02-22 18:52:13 +0100

    Update sketch.js

--- dontpullmeout/sketch.js
+++ dontpullmeout/sketch.js
@@ -1,10 +1,16 @@
+
+
 function setup() {
     createCanvas(900, 600);
-    
+    noCursor();
 }
 
 function draw() {
-    background(220);
+    background(235);
+    noFill()
+    strokeWeight(300);
+    stroke(0, 200);
+    rect(0,0, width, height);
     bubble.display();
     bubble.move();
     
@@ -12,21 +18,28 @@ function draw() {
 }
 
 var bubble = {
-    x: mouseX,
-    y: mouseY,
+    x: 0,
+    y: 0,
     display: function(){
         stroke(25);
         strokeWeight(6);
         noFill();
         ellipse(this.x, this.y, 24, 24);
     },
+   
     move: function(){
-        this.x = this.x + random(-1, 1);
-        this.y = this.y + random(-1, 1);
-        if(this.y >= width) {
-            this.x = this.x + random(-1, 1);
-            this.y = this.y + random(-1, 1);
+        this.x = mouseX;
+        this.y = mouseY;
+        this.x = this.x + random(-2, 2);
+        this.y = this.y + random(-2, 2);
+        if(this.x >= 750 && this.x <= 900) {
+            this.x = mouseX -300;
+        } if(this.x <= 150 && this.x >= 0) {
+          this.x = mouseX +300;  
+        } if(this.y <= 150 && this.y >= 0) {
+            this.y = mouseY +250;
+        } if(this.y >= 450 && this.y <= 600) {
+            this.y = mouseY -250; 
         }
     }
-        
 }
